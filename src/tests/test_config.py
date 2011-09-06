22193413c9dce9e9bed4f6073ad5b7b0f40c83e5
Arlukin <daniel@cybercow.se>
2011-09-06 21:33:24 +0200

    Added unit tests for Config class.

--- src/tests/test_config.py
+++ src/tests/test_config.py
@@ -14,16 +14,39 @@ __status__ = "Test"
 import unittest
 from test import test_support
 
-from oand import Config
+from config import Config
 
 class TestConfig(unittest.TestCase):
   serverName = "this-is-a-server-name"
-  
+  serverDomainName = "www.cybercow.se"
+  serverPort = "4000"
+  bffNode = "node1.cybecow.se"
+
   def setUp(self):
-    self.config = Config(self.serverName)
+    self.config = Config(self.serverName,
+      self.serverDomainName,
+      self.serverPort,
+      self.bffNode
+    )
 
   def test_getServerName(self):
     self.assertEqual(self.config.getServerName(), self.serverName)
+
+  def test_getServerDomainName(self):
+    self.assertEqual(self.config.getServerDomainName(), self.serverDomainName)
+
+  def test_getServerPort(self):
+    self.assertEqual(self.config.getServerPort(), self.serverPort)
+
+  def test_getBffNode(self):
+    self.assertEqual(self.config.getBffNode(), self.bffNode)
+
+class TestConfigLoadFromFile(TestConfig):
+  def setUp(self):
+    self.config = Config.fromFilename("./tests/oand.cfg")
+
+  def test_fileNotExist(self):
+    self.assertRaises(IOError, Config.fromFilename, ("file-not-exist.cfg"))
 
 if __name__ == '__main__':
   unittest.main()
