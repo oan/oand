18ae807dd7b8c9115e70db7c37a8878994f1ca73
Arlukin <daniel@cybercow.se>
2011-10-03 21:52:49 +0200

    Disabled all networknode unittests

--- src/tests/test_networknode.py
+++ src/tests/test_networknode.py
@@ -13,34 +13,34 @@ __status__ = "Test"
 
 import unittest
 
-from networknode import NetworkNode
+#from networknode import NetworkNode
 
-class TestNetworkNode(unittest.TestCase):
-    _network_node = None
+#class TestNetworkNode(unittest.TestCase):
+#    _network_node = None
 
-    def setUp(self):
-        self._network_node = NetworkNode('leet-server', 'leetsrv.com', '1337')
+#    def setUp(self):
+#        self._network_node = NetworkNode('leet-server', 'leetsrv.com', '1337')
 
-    def test_defaults(self):
-        self.assertEqual(self._network_node.get_last_heartbeat(), "2006-06-06 06:06:06")
+#    def test_defaults(self):
+#        self.assertEqual(self._network_node.get_last_heartbeat(), "2006-06-06 06:06:06")
 
-    def test_touch_last_heartbeat(self):
-        self._network_node.touch_last_heartbeat()
-        self.assertEqual(self._network_node.is_heartbeat_expired(), False)
-        self.assertEqual(self._network_node.is_node_inactive(), False)
+#    def test_touch_last_heartbeat(self):
+#        self._network_node.touch_last_heartbeat()
+#        self.assertEqual(self._network_node.is_heartbeat_expired(), False)
+#        self.assertEqual(self._network_node.is_node_inactive(), False)
 
-    def test_is_heartbeat_expired(self):
-        self._network_node.set_last_heartbeat("2011-01-01 01:01:01")
-        self.assertEqual(self._network_node.is_heartbeat_expired(), True)
-        self.assertEqual(self._network_node.is_node_inactive(), True)
+#    def test_is_heartbeat_expired(self):
+#        self._network_node.set_last_heartbeat("2011-01-01 01:01:01")
+#        self.assertEqual(self._network_node.is_heartbeat_expired(), True)
+#        self.assertEqual(self._network_node.is_node_inactive(), True)
 
-    def test_get_last_heartbeat(self):
-        heartbeat = "2011-01-01 01:01:01"
-        self._network_node.set_last_heartbeat(heartbeat)
-        self.assertEqual(self._network_node.get_last_heartbeat(), heartbeat)
+#    def test_get_last_heartbeat(self):
+#        heartbeat = "2011-01-01 01:01:01"
+#        self._network_node.set_last_heartbeat(heartbeat)
+#        self.assertEqual(self._network_node.get_last_heartbeat(), heartbeat)
 
-    def test_is_valid(self):
-        self.assertEqual(self._network_node.is_valid(), True)
+#    def test_is_valid(self):
+#        self.assertEqual(self._network_node.is_valid(), True)
 
 if __name__ == '__main__':
     unittest.main()
