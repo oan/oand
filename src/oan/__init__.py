3c8e3637a56b674fdbe1b2906cf26d40ccd07ac7
Arlukin <daniel@cybercow.se>
2012-02-02 14:37:11 +0100

    Standard file header in all __init__.py

--- src/oan/__init__.py
+++ src/oan/__init__.py
@@ -1,4 +1,14 @@
-# __init__.py
+"""
+Constanst available via package oan.
+
+"""
+
+__author__ = "daniel.lindh@cybercow.se"
+__copyright__ = "Copyright 2011, Amivono AB"
+__maintainer__ = "daniel.lindh@cybercow.se"
+__license__ = "We pwn it all."
+__version__ = "0.1"
+__status__ = "Test"
 
 import os
 
@@ -13,6 +23,3 @@ ETC_DIR = BASE_DIR + "etc/"
 
 # Log files
 LOG_DIR = BASE_DIR + "log/"
-
-# todo: Should be moved to dispatcher??
-#from manager import *
--- src/oan/database/__init__.py
+++ src/oan/database/__init__.py
@@ -1,3 +1,11 @@
-# __init__.py
+"""
+The mere presence of this file makes the dir a package.
 
-from database import OANDatabase
+"""
+
+__author__ = "daniel.lindh@cybercow.se"
+__copyright__ = "Copyright 2011, Amivono AB"
+__maintainer__ = "daniel.lindh@cybercow.se"
+__license__ = "We pwn it all."
+__version__ = "0.1"
+__status__ = "Test"
