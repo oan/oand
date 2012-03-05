e4bde9f951aff210c555e22f3986bc318dcdb901
Martin Palmér <martin.palmer.develop@gmail.com>
2012-03-05 16:56:36 +0100

    OANThread class that is a demon thread with a enable_shutdown option

---
+++ src/oan/util/thread.py
@@ -0,0 +1,30 @@
+#!/usr/bin/env python
+'''
+OANThread
+
+'''
+
+__author__ = "martin.palmer.develop@gmail.com"
+__copyright__ = "Copyright 2011, Amivono AB"
+__maintainer__ = "martin.palmer.develop@gmail.com"
+__license__ = "We pwn it all."
+__version__ = "0.1"
+__status__ = "Test"
+
+from threading import Event, Thread
+
+class OANThread(Thread):
+
+    def __init__(self):
+        Thread.__init__(self)
+        self.daemon = True
+        self.shutdown_event = Event()
+
+    def enable_shutdown(self):
+        self.shutdown_event.set()
+
+    def disable_shutdown(self):
+        self.shutdown_event.clear()
+
+    def join(self):
+        self.shutdown_event.wait()
